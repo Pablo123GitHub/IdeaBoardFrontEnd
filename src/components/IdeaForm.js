6a492cea5e4bcec2cc2dbee5560f3f00939859e5
Pablo <paul.ladiv123@gmail.com>
2018-09-08 15:52:02 +0100

    add function handlers in order to set value for title and body of the form

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -6,16 +6,27 @@ class IdeaForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            title: this.props.idea.title,
+            body: this.props.idea.body
         }
+    }
+
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
     }
 
     render() {
         return (
             <div className='tile' >
            <form>
-               <input className='input' type='text' name='title' placeholder='Enter a title'/>
-               <textarea className='input' name='body' placeholder='Describe idea'></textarea>
+               <input className='input' type='text' name='title' placeholder='Enter a title'
+               value={this.state.title} onChange={this.handleInput} />
+               <textarea className='input' name='body' placeholder='Describe idea'
+               value={this.state.body} onChange={this.handleInput}>
+
+               </textarea>
            </form>
 
             </div>
