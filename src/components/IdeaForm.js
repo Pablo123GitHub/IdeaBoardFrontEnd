68f14395aa926d66867d35f3b624fe45a6c3095f
Pablo <paul.ladiv123@gmail.com>
2018-09-08 16:04:44 +0100

    add handleBlur so each IdeaForm can update the Ideas API in the DB

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 
 class IdeaForm extends Component {
@@ -17,10 +18,22 @@ class IdeaForm extends Component {
         })
     }
 
+    handleBlur = () => {
+        const idea = {title: this.state.title, body : this.state.body}
+        axios.put(
+            `http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
+            {idea: idea}
+        )
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => console.log(err))
+    }
+
     render() {
         return (
             <div className='tile' >
-           <form>
+           <form onBlur={this.handleBlur}>
                <input className='input' type='text' name='title' placeholder='Enter a title'
                value={this.state.title} onChange={this.handleInput} />
                <textarea className='input' name='body' placeholder='Describe idea'
